406cc076e66ff5ebce74656335d9445389c8f55a
code with srnagur <acchinagur@gmail.com>
2024-07-20 09:02:56 +0530

    push hub

---
+++ dumHUB/src/test/java/organ/CreateOrgan.java
@@ -0,0 +1,11 @@
+package organ;
+
+public class CreateOrgan {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("create organname successfully");
+
+	}
+
+}
